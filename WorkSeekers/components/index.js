c23c48bad09c49b65d4ca84b8267a492c8e2b799
mar19a <mariano@bu.edu>
2023-05-24 19:14:35 -0400

    Working on index.js file

--- WorkSeekers/components/index.js
+++ WorkSeekers/components/index.js
@@ -11,19 +11,3 @@ import { default as JobTabs } from "./jobdetails/tabs/Tabs";
 import { default as JobAbout } from "./jobdetails/about/About";
 import { default as JobFooter } from "./jobdetails/footer/Footer";
 import Specifics from "./jobdetails/specifics/Specifics";
-
-// common
-import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
-
-export {
-  ScreenHeaderBtn,
-  Welcome,
-  Nearbyjobs,
-  Popularjobs,
-  Company,
-  JobTabs,
-  JobAbout,
-  JobFooter,
-  Specifics,
-  NearbyJobCard
-};
