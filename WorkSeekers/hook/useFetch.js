1434a18840b19aba72703a5a7d97b699c098c768
mar19a <mariano@bu.edu>
2023-05-25 00:49:14 -0400

    API key problem

--- WorkSeekers/hook/useFetch.js
+++ WorkSeekers/hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '36e6048280msh34f22b8f6977114p1432ecjsn47f8721aacb1',
+      "X-RapidAPI-Key": '468155d14cmshdb119f697ad0e99p151e0ejsn21dc85e6c284',
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
