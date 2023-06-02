91c33aa52cde959d0def122366c3cc2e204cea5a
mar19a <mariano@bu.edu>
2023-06-02 00:16:31 -0400

    Router push

--- WorkSeekers/components/home/nearby/Nearbyjobs.jsx
+++ WorkSeekers/components/home/nearby/Nearbyjobs.jsx
@@ -32,3 +32,5 @@ const Nearbyjobs = () => {
           data?.map((job) => (
             <NearbyJobCard
               job={job}
+              key={`nearby-job-${job.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
