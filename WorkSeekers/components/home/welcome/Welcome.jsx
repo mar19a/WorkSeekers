b8cf7b09ef5190bfa3bc5a8fb10301c409109901
mar19a <mariano@bu.edu>
2023-06-01 23:57:55 -0400

    Added different job types

--- WorkSeekers/components/home/welcome/Welcome.jsx
+++ WorkSeekers/components/home/welcome/Welcome.jsx
@@ -13,4 +13,7 @@ import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-Time", "Part-Time", "Freelancer", "Internship", "Temporary", "Contract", "Commissions", "Volunteer"];
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
 
