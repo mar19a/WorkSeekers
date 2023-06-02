4b74cc13fe38e467fffde1d06e232c69d330fff2
mar19a <mariano@bu.edu>
2023-06-02 00:12:30 -0400

    Change size of cards container

--- WorkSeekers/components/home/popular/popularjobs.style.js
+++ WorkSeekers/components/home/popular/popularjobs.style.js
@@ -16,4 +16,14 @@ const styles = StyleSheet.create({
     fontFamily: FONT.medium,
     color: COLORS.primary,
   },
+  headerBtn: {
+    fontSize: SIZES.medium,
+    fontFamily: FONT.medium,
+    color: COLORS.gray,
+  },
+  cardsContainer: {
+    marginTop: SIZES.medium,
+  },
+});
 
+export default styles;
