ed0456684e42d07913591a7022c89ccfaec16f84
mar19a <mariano@bu.edu>
2023-06-01 23:33:52 -0400

    TouchableOpacity style

--- WorkSeekers/components/jobdetails/tabs/Tabs.jsx
+++ WorkSeekers/components/jobdetails/tabs/Tabs.jsx
@@ -4,3 +4,10 @@ import { TouchableOpacity, FlatList, Text, View } from "react-native";
 import styles from "./tabs.style";
 import { SIZES } from "../../../constants";
 
+function TabButton({ name, activeTab, onHandleSearchType }) {
+  return (
+    <TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onHandleSearchType}
+    >
+      
