8774d55bdfe400becc446b1884ff7b0d1606311c
mar19a <mariano@bu.edu>
2023-06-02 00:11:02 -0400

    Header style

--- WorkSeekers/components/home/popular/popularjobs.style.js
+++ WorkSeekers/components/home/popular/popularjobs.style.js
@@ -16,14 +16,3 @@ const styles = StyleSheet.create({
     fontFamily: FONT.medium,
     color: COLORS.primary,
   },
-  headerBtn: {
-    fontSize: SIZES.medium,
-    fontFamily: FONT.medium,
-    color: COLORS.gray,
-  },
-  cardsContainer: {
-    marginTop: SIZES.medium,
-  },
-});
-
-export default styles;
