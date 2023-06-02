430dfe3de8f756bf585f418566202cfecf870b70
mar19a <mariano@bu.edu>
2023-06-02 00:07:57 -0400

    Small changes

--- WorkSeekers/components/home/popular/Popularjobs.jsx
+++ WorkSeekers/components/home/popular/Popularjobs.jsx
@@ -27,38 +27,3 @@ const Popularjobs = () => {
     setSelectedJob(item.job_id);
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size='large' color={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong</Text>
-        ) : (
-          <FlatList
-            data={data}
-            renderItem={({ item }) => (
-              <PopularJobCard
-                item={item}
-                selectedJob={selectedJob}
-                handleCardPress={handleCardPress}
-              />
-            )}
-            keyExtractor={(item) => item.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
-          />
-        )}
-      </View>
-    </View>
-  );
-};
-
-export default Popularjobs;
