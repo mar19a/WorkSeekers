5e8c33607d0250d67c39fd0cf58c83d52b38b709
mar19a <mariano@bu.edu>
2023-06-01 23:36:42 -0400

    Finished tabs.jsx

--- WorkSeekers/components/jobdetails/tabs/Tabs.jsx
+++ WorkSeekers/components/jobdetails/tabs/Tabs.jsx
@@ -21,3 +21,19 @@ const Tabs = ({ tabs, activeTab, setActiveTab }) => {
       <FlatList
         data={tabs}
         horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
+      />
+    </View>
+  );
+};
+
+export default Tabs;
