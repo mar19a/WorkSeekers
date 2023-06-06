baa1f6b8aba6410a69d7255605a64b49a7e73d97
mar19a <mariano@bu.edu>
2023-06-05 21:13:43 -0400

    Job location

--- WorkSeekers/components/common/cards/popular/PopularJobCard.jsx
+++ WorkSeekers/components/common/cards/popular/PopularJobCard.jsx
@@ -22,3 +22,21 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       </TouchableOpacity>
       <Text style={styles.companyName} numberOfLines={1}>
         {item.employer_name}
+      </Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.publisher(selectedJob, item)}>
+            {item?.job_publisher} -
+          </Text>
+          <Text style={styles.location}> {item.job_country}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default PopularJobCard;
