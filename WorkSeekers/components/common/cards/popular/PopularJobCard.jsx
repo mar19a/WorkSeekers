9592ea0050e98aebac0852faaea1a2f1c2632b28
mar19a <mariano@bu.edu>
2023-06-05 21:12:41 -0400

    Working on opacity

--- WorkSeekers/components/common/cards/popular/PopularJobCard.jsx
+++ WorkSeekers/components/common/cards/popular/PopularJobCard.jsx
@@ -3,4 +3,10 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import styles from "./popularjobcard.style";
 import { checkImageURL } from "../../../../utils";
 
-const PopularJobCard = 
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
