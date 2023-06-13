42ad54a7fc42a93d2a0072c0d4ca8fd55a59c2b8
mar19a <mariano@bu.edu>
2023-06-12 23:42:24 -0400

    Container style

--- WorkSeekers/components/common/cards/nearby/nearbyjobcard.style.js
+++ WorkSeekers/components/common/cards/nearby/nearbyjobcard.style.js
@@ -14,34 +14,3 @@ const styles = StyleSheet.create({
     ...SHADOWS.medium,
     shadowColor: COLORS.white,
   },
-  logoContainer: {
-    width: 50,
-    height: 50,
-    backgroundColor: COLORS.white,
-    borderRadius: SIZES.medium,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logImage: {
-    width: "70%",
-    height: "70%",
-  },
-  textContainer: {
-    flex: 1,
-    marginHorizontal: SIZES.medium,
-  },
-  jobName: {
-    fontSize: SIZES.medium,
-    fontFamily: "DMBold",
-    color: COLORS.primary,
-  },
-  jobType: {
-    fontSize: SIZES.small + 2,
-    fontFamily: "DMRegular",
-    color: COLORS.gray,
-    marginTop: 3,
-    textTransform: "capitalize",
-  },
-});
-
-export default styles;
