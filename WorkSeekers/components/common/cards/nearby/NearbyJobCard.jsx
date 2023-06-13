56761295621b7672de90e4f520d6bd6475db03f1
mar19a <mariano@bu.edu>
2023-06-12 23:40:56 -0400

    Contain job name, job type, employment type, and employer logo

--- WorkSeekers/components/common/cards/nearby/NearbyJobCard.jsx
+++ WorkSeekers/components/common/cards/nearby/NearbyJobCard.jsx
@@ -13,3 +13,15 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
               ? job.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
+          resizeMode='contain'
+          style={styles.logImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job?.job_title}
+        </Text>
+
+        <Text style={styles.jobType}>{job?.job_employment_type}</Text>
+ 
