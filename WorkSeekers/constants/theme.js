2532f2f0b2b9020ae19fc8d1c7f0b8aa4c0acc9e
mar19a <mariano@bu.edu>
2023-06-12 23:47:54 -0400

    Small shadows

--- WorkSeekers/constants/theme.js
+++ WorkSeekers/constants/theme.js
@@ -25,3 +25,14 @@ const SIZES = {
   xxLarge: 32,
 };
 
+const SHADOWS = {
+  small: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
